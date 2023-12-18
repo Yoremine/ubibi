0263bf30efb49acbea1906b3f4cabedb240b4e75
Yoremine <144691124+Yoremine@users.noreply.github.com>
2023-12-18 17:28:53 +0300

    1

---
+++ scripts/initialCards.js
@@ -0,0 +1,28 @@
+const initialCards = [
+    {
+      name: 'Крик',
+      link: 'https://i.pinimg.com/564x/7b/60/7c/7b607c3308031530388f0e5b6b6a597b.jpg'
+    },
+    {
+      name: 'Мона Лиза',
+      link: 'https://i.pinimg.com/564x/36/e0/e7/36e0e78e9c51bf5e0d076904534a6e05.jpg'
+    },
+    {
+      name: 'Подсолнухи',
+      link: 'https://i.pinimg.com/564x/69/76/b6/6976b6546eee8c7b11f9a9ac00770e4a.jpg'
+    },
+    {
+      name: 'Котята',
+      link: 'https://i.pinimg.com/564x/99/b9/1f/99b91f061960fd398860f764ccd7d058.jpg'
+    },
+    {
+      name: 'Девушка в поле',
+      link: 'https://i.pinimg.com/564x/a2/8b/b2/a28bb232d307ef72f33692b0c44ebd2f.jpg'
+    },
+    {
+      name: 'Виктор Цой',
+      link: 'https://i.pinimg.com/564x/3c/ee/34/3cee34f12f9da9d3cbf91e0ef90b2fa9.jpg'
+    }
+  ];
+  
+  export default initialCards;
